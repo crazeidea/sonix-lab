43fc9916f100a29741e7be2f59ea61f177dec9e0
최강훈 <ganghun@lepisode.team>
2025-09-10 16:57:59 +0900

    chore: replaced logo

---
+++ src/app/components/logo/logo.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './logo.svg',
+  styles: `
+    svg {
+      width: 100%;
+      height: auto;
+    }
+  `,
+})
+export class Logo {}
